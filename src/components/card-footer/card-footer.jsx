f5a43dec7da0c02c0972d0b9e70aa78fc66458ea
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-25 00:10:27 +0300

    adding the card footer

--- src/components/card-footer/card-footer.jsx
+++ src/components/card-footer/card-footer.jsx
@@ -1,6 +1,18 @@
+import facebookIcon from "../shared/icons/facebook.svg";
+import githubIcon from "../shared/icons/github.svg";
+import instagramIcon from "../shared/icons/instagram.svg";
+import twitterIcon from "../shared/icons/twitter.svg";
+import SocialLink from "../shared/social-link";
 import "./card-footer.css";
 function CardFooter() {
-	return <div className="card-footer"></div>;
+	return (
+		<div className="card-footer">
+			<SocialLink icon={twitterIcon} />
+			<SocialLink icon={facebookIcon} />
+			<SocialLink icon={instagramIcon} />
+			<SocialLink icon={githubIcon} />
+		</div>
+	);
 }
 
 export default CardFooter;
