b00d7eb22e6d25e7bc9ba39d0b3e477f9f9cd063
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-24 23:20:00 +0300

    adding the card content about and interest section

--- src/components/card-content/card-content.jsx
+++ src/components/card-content/card-content.jsx
@@ -16,6 +16,19 @@ function CardContent() {
 					theme="blue"
 				/>
 			</div>
+			<h2 className="card-content-section-title">About</h2>
+			<p className="card-content-section-content">
+				I am a frontend developer with a particular interest in making
+				things simple and automating daily tasks. I try to keep up with
+				security and best practices, and am always looking for new
+				things to learn.
+			</p>
+			<h2 className="card-content-section-title">Interests</h2>
+			<p className="card-content-section-content">
+				Food expert. Music scholar. Reader. Internet fanatic. Bacon
+				buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee
+				fanatic.
+			</p>
 		</div>
 	);
 }
